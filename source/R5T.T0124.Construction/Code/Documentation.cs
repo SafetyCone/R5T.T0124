ea892bb1434aac75ad78b137fa89c29ee8e77602
davidcoats <david@getsafetycone.com>
2022-01-02 23:59:15 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0124.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0124.Construction
+{
+    /// <summary>
+    /// Construction project for ISearchPattern and ISearchPatternOperator extension method bases..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0124/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0124
+{
+    /// <summary>
+    /// ISearchPattern and ISearchPatternOperator extension method bases.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
